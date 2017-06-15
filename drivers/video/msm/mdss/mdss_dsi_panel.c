1c5cfd758726f22b0d3aeb4ec9b195cb0d0e41a6
Shoaib0597 <Shoaib0595@gmail.com>
2017-06-15 15:53:03 +0530

    LazyPlug: Remove unnecessary Codes
    Signed-off-by: Shoaib0597 <Shoaib0595@gmail.com>
    
    In the commit titled "lazyplug: Only use super aggressive profile when screen is off", a new boolean variable "display_on" has been introduced. However, it hasn't been used in any Code of LazyPlug. Hence, I have removed this unnecessary Code.

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -53,8 +53,6 @@ char g_lcm_id[128];
 
 extern void lazyplug_enter_lazy(bool enter, bool video);
 
-bool display_on = true;
-
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
@@ -694,7 +692,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
        set_power_suspend_state_panel_hook(POWER_SUSPEND_INACTIVE);
 #endif
 
-	display_on = true;
 	lazyplug_enter_lazy(false, false);
 
 	pinfo = &pdata->panel_info;
@@ -796,7 +793,6 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
        set_power_suspend_state_panel_hook(POWER_SUSPEND_ACTIVE);
 #endif
 
-	display_on = false;
 	lazyplug_enter_lazy(true, false);
 
 end:
