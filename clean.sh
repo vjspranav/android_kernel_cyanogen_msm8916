2857480b63ef717483d3ae99700bf62ef290b3fe
Panchajanya1999 <rsk52959@gmail.com>
2017-11-26 13:40:53 +0530

    Added A Build Script and A Clean Script
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

---
+++ clean.sh
@@ -0,0 +1,8 @@
+echo "Started Cleaning Kernel Sources from AzurE Oreo" 
+export CROSS_COMPILE=/home/panchajanya/Kernel/Toolchains/aarch64-linux-android-7.2.1-uber/bin/aarch64-linux-android-
+export ARCH=arm64
+export SUBARCH=arm64
+make clean && make mrproper
+echo "Started Cleaning Anykernel Sources for AzurE Oreo"
+rm -rf ../anykernel/dt.img
+rm -rf ../anykernel/zImage
