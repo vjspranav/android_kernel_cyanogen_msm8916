86a73c76043d847345a2051be4ed42dc75f16839
Panchajanya1999 <rsk52959@gmail.com>
2017-12-05 16:22:42 +0530

    Modified Build Script a bit, and added Device name :)
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-export KBUILD_BUILD_USER="macOS_sierra"
-export KBUILD_BUILD_HOST="root"
+export KBUILD_BUILD_USER="root"
+export KBUILD_BUILD_HOST="Macintosh_High-Sierra"
 export CROSS_COMPILE=/home/panchajanya/Kernel/Toolchains/aarch64-linux-android-7.2.1-uber/bin/aarch64-linux-android-
 export ARCH=arm64
 export SUBARCH=arm64
 make clean && make mrproper
-rm -rf ../anykernel/dt.img
+rm -rf anykernel/dt.img
 #rm -rf ../anykernel/modules/wlan.ko
-rm -rf ../anykernel/zImage
-ccache -c
+rm -rf anykernel/zImage
+ccache -M 10G
 BUILD_START=$(date +"%s")
 KERNEL_DIR=$PWD
 DTBTOOL=$KERNEL_DIR/tools/dtbToolCM
@@ -23,12 +23,17 @@ make -j8
 echo "Making dt.img"
 $DTBTOOL -2 -o $KERNEL_DIR/arch/arm64/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
 echo "Done"
-BUILD_END=$(date +"%s")
-DIFF=$(($BUILD_END - $BUILD_START))
-BUILD_TIME=$(date +"%Y%m%d-%T")
-echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol"
+export IMAGE=$KERNEL_DIR/arch/arm64/boot/Image
+if [[ ! -f "${IMAGE}" ]]; then
+    echo -e "Build failed :P. Check errors!";
+    break;
+else
+BUILD_END=$(date +"%s");
+DIFF=$(($BUILD_END - $BUILD_START));
+BUILD_TIME=$(date +"%Y%m%d-%T");
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol";
 echo "Movings Files"
-cd ../anykernel
+cd anykernel
 mv $KERNEL_DIR/arch/arm64/boot/Image zImage
 mv $KERNEL_DIR/arch/arm64/boot/dt.img dt.img
 #mv $KERNEL_DIR/drivers/staging/prima/wlan.ko modules/wlan.ko
@@ -36,4 +41,6 @@ echo "Making Zip"
 zip -r AzurE-Oreo-$BUILD_TIME *
 cd ..
 mv anykernel/AzurE-Oreo-$BUILD_TIME.zip /home/panchajanya/Kernel/Zips/Azure-Builds/Oreo-Builds/AzurE-Oreo-$BUILD_TIME.zip
+echo -e "Kernel is named as $yellow AzurE-Oreo-$BUILD_TIME.zip $nocol and can be found at $yellow /home/panchajanya/Kernel/Zips/Azure-Builds/Oreo-Builds.$nocol"
+fi
 cd 
